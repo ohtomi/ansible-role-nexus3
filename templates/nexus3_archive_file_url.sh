ba56741b307cc651812cf603fac00811cf033108
ohtomi <ohtomi.kenichi@gmail.com>
2016-11-16 01:53:24 +0900

    Initial commit

---
+++ templates/nexus3_archive_file_url.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# curl -Is http://download.sonatype.com/nexus/3/{{ nexus3_version }}-unix.tar.gz
+echo "https://sonatype-download.global.ssl.fastly.net/nexus/3/{{ nexus3_version }}-unix.tar.gz"
