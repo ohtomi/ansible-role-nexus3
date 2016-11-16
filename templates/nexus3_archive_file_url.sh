a4850ed4d16e9b42a400a7dd7f010d1681aeba66
ohtomi <ohtomi.kenichi@gmail.com>
2016-11-17 01:15:19 +0900

    Resolve true download url using curl

--- templates/nexus3_archive_file_url.sh
+++ templates/nexus3_archive_file_url.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-# curl -Is http://download.sonatype.com/nexus/3/{{ nexus3_version }}-unix.tar.gz
-echo "https://sonatype-download.global.ssl.fastly.net/nexus/3/{{ nexus3_version }}-unix.tar.gz"
+curl_result=`curl -Is $1`
+
+curl_rc=$?
+if [ $curl_rc -ne 0 ]; then
+  echo "http error. url: $1"
+  exit $curl_rc
+fi
+
+location_header_found=`echo "$curl_result" | sed -n 's/^Location: \(.*\)/\1/p'`
+if [ "$location_header_found" == "" ]; then
+  echo $1
+else
+  bash $0 $location_header_found
+fi
